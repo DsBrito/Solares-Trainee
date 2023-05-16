400aa67864ddbfb3cd95fc2610c85a670da9a32b
dsbrito <dionatas.ufes@gmail.com>
2023-05-16 19:50:20 -0300

    add: 🦔 site 🦔

--- desafio-telemetria/desafio/src/pages/index.tsx
+++ desafio-telemetria/desafio/src/pages/index.tsx
@@ -1,12 +1,25 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className = "bg-blue-400 w-full h-screen flex ">
+    <div style={{ backgroundColor: '#050758' }}>
+            <img 
+            src="https://raw.githubusercontent.com/DsBrito/Solares-Trainee/main/images/itens/solares.png?token=GHSAT0AAAAAACCXROE6MWEDUCSK4LH6V6HQZDEAS7A" alt="solares"
+            height={200}
+            width={200}
+            className='logo'
 
-    </div>   
+             />
+
+          <button id="change-light">
+              <img src="https://raw.githubusercontent.com/DsBrito/Solares-Trainee/main/images/itens/sun.png?token=GHSAT0AAAAAACCXROE77MU6L6UPI6RMOF3IZDEATGA" alt="sun-image" 
+                height={200}
+                width={200} 
+                
+                className='botton-image'
+                />
+          </button>
+    </div>
   )
 }
