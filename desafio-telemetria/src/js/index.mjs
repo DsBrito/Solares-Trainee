093ad8d872ffd61c8be77993b1451acbee0ccf1e
DsBrito <80075307+DsBrito@users.noreply.github.com>
2023-05-19 12:07:17 -0300

    add :hatched_chick: gráfico e variaveis :hatched_chick:
    gráfico e variáveis

--- desafio-telemetria/src/js/index.mjs
+++ desafio-telemetria/src/js/index.mjs
@@ -36,6 +36,48 @@ lightBotton.addEventListener("click", () => {
 })
 
 
+//grafico 1
+// Variáveis globais
+var chart;
+var chartData = [];
+
+// Função para inicializar o gráfico
+function initChart() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'VELOCIDADE DO BARCO',
+        data: chartData,
+        borderColor: 'white',
+        backgroundColor: 'transparent',
+      }]
+    },
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+// Função para adicionar novos dados ao gráfico
+function addDataToChart(newData) {
+  chartData.push(newData);
+  if (chartData.length > 10000) {
+    chartData.shift();
+  }
+
+  chart.data.labels = Array.from({ length: chartData.length }, (_, i) => i + 1);
+  chart.update();
+}
+
+//grafico 2
+
 //Variaveis boleanas ON/OFF
 const toggleButton1 = document.getElementById('toggleButton1');
 const toggleButton2 = document.getElementById('toggleButton2');
@@ -43,13 +85,16 @@ const toggleButton2 = document.getElementById('toggleButton2');
 
 // Exemplo de alteração nas variáveis booleanas
 
+//escrita na tela
+var info = document.getElementById("info");
+
 
 // connect to the websocket server
 const socket = io.connect('https://telemetria-trainee-2023.onrender.com');
-console.log('entrei aqui 1: ')
+//console.log('entrei aqui 1: ')
 // this message is triggered when the websocket gets connected
 socket.on('connect', () => console.log('websocket connected!'))
-console.log('entrei aqui 2: ')
+//console.log('entrei aqui 2: ')
 
 // this message is triggered when the websocket gets disconnected
 socket.on('disconnect', () => console.log('websocket disconnect'))
@@ -57,10 +102,22 @@ console.log('entrei aqui 3: ')
 
 // receive a message from the server on the info 'channel'
 socket.on("info", data => {
-      console.log('entrei aqui 4: ')
-
-      console.log('data: ', data.estadoStringSolar1)
-      console.log('data: ', data.estadoStringSolar2)
+info.innerHTML = "Resultado:<br>" +
+                "<br>  -> corrente do motor: " + data.correnteMotor +
+                "<br>  -> corrente da baterias: " + data.correnteBaterias +
+                "<br>  -> temperatura: " + data.temperatura +
+                "<br>  -> umidade: " + data.umidade +
+                "<br>  -> tensão da alimentação PCB: " + data.tensaoAlimentacaoPCB +
+                "<br>  -> estado da string Solar1: " + data.estadoStringSolar1 +
+                "<br>  -> estado da string Solar2: " + data.estadoStringSolar2 +
+                "<br>  -> tensão de saída MPPT: " + data.tensaoAlimentacaoPCB +
+                "<br>  -> tensao de entrada MPPT: " + data.tensaoEntradaMPPT +
+                "<br>  -> corrente MPPT: " + data.correnteMPPT +
+                "<br>  -> velocidade do barco: " + data.velocidadeBarco +
+                "<br>  -> latitude: " + data.latitude +
+                "<br>  -> longitude: " + data.longitude +
+                "<br>  -> update At: " + data.updateAt
+              ;
 
     if(data.estadoStringSolar1 == 1 && data.estadoStringSolar2==1){
       toggleButton1.textContent = 'ON'
@@ -93,4 +150,15 @@ socket.on("info", data => {
       toggleButton2.classList.toggle('on');
 
     }
+
+
+//grafico
+  console.log('velocidadeBarco: ', data.velocidadeBarco);
+  
+  addDataToChart(data.velocidadeBarco);
+  //addDataToChart(data.temperatura);
+
+
 });
+
+initChart();
