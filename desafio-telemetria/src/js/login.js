abbfbe53ecb9dd0522123c507b6e166769cad6d8
qaSA <>
2023-05-26 11:41:13 -0300

    att :: 🦝 comentários 🦝
    Comentários referente ao login adicionados.

--- desafio-telemetria/src/js/login.js
+++ desafio-telemetria/src/js/login.js
@@ -1,30 +1,48 @@
 
+/**
+ * Aqui se encontra do o script simples de verificação do login, tendo
+ * tratamento de erros (login e senha errados ou em branco), também
+ * há uma função referente a visibilidade da senha inserida pelo usuário
+ */
 
 
+//verificação da visibilidade da senha com base no estado do checkbox
+  const passwordInput = document.getElementById('password');
+  const revealPasswordCheckbox = document.getElementById('revealPassword');
+
+  revealPasswordCheckbox.addEventListener('click', function() {
+
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+    } else {
+      passwordInput.type = 'password';
+    }
+  });
+
+//verificação de entrada pelo teclado (tecla "Enter")
   document.getElementById("password").addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
       login();
     }
   });
 
+
+//Lógica de login, utilizando blocos condicionais simples para 
  function login() {
-
-
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let remember = document.getElementById("remember").checked;
 
-    // Authentication logic
+    //lógica para autenticação
     if((username=="daviagatti@gmail.com" && password=="telemetria23" )|| (username=="guiefgen@gmail.com" && password=="telemetria23")||(username=="fernandorr1108@gmail.com" && password=="telemetria23")||(username=="dionatas-solares@gmail.com" && password=="telemetria")){
         if (remember) {
             localStorage.setItem("username", username);
         }
         //alert("Login bem-sucedido!");
         window.location="index2.html";
-
         } 
+    //lógica para a correção de erros.
     else{
-
         if (username =='' && password ==''){
             alert("Usuário e senha não inseridos. Tente novamente.");
         }
@@ -38,9 +56,6 @@
             alert("Nome de usuário ou senha incorretos. Tente novamente.");
             document.getElementById("password").value = ""; // Limpa o campo de senha
         }
-
         window.location="login.html";
-
-
     }
 }
